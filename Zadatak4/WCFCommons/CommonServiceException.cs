e4cc59746ba0b2abcc9c9d51016960fab8ff9351
Studentko <you@example.com>
2018-11-06 12:58:10 +0100

    ICommonService and other stuff

---
+++ Zadatak4/WCFCommons/CommonServiceException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFCommons
+{
+    public class CommonServiceException
+    {
+        string Message { get; set; }
+    }
+}
---
+++ Zadatak4/WCFCommons/ICommonService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFCommons
+{
+    [ServiceContract]
+    public interface ICommonService
+    {
+        [OperationContract]
+        [FaultContract(typeof(CommonServiceException))]
+        void Create(string fileName);
+
+        [OperationContract]
+        [FaultContract(typeof(CommonServiceException))]
+        void Modify(string fileName, string data, EModifyType modifyType);
+
+        [OperationContract]
+        [FaultContract(typeof(CommonServiceException))]
+        string Read(string fileName);
+
+        [OperationContract]
+        [FaultContract(typeof(CommonServiceException))]
+        void Delete(string fileName);
+
+    }
+}
