d63d970f392fee72705975657b0a18f7b6304359
Studentko <you@example.com>
2018-11-06 11:51:47 +0100

    Project structure

---
+++ Zadatak4/SecureProxy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
