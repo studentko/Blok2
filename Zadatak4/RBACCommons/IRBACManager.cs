f7c541d1c5e3f695582383fcb847101aaf5ebb69
Kosta Curcic <kosta.curcic.96@gmail.com>
2018-11-06 14:28:54 +0100

    Added RBAC Interface

---
+++ Zadatak4/RBACCommons/IRBACManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RBACCommons
+{
+    public interface IRBACManager
+    {
+        void AddGroup(string group);
+        void AddPermToGroup(string group, string perm);
+        bool IsGroupAllowed(string group, string perm);
+        List<string> GetPermsForGroup(string group);
+        Dictionary<string, List<string>> GetAll();
+    }
+}
