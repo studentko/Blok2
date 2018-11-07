1a0d0c5fc65afe111eef7cea5fa4c919d03eade5
Kosta Curcic <kosta.curcic.96@gmail.com>
2018-11-07 11:01:24 +0100

    CommonService with file management

--- Zadatak4/SecureHost/Program.cs
+++ Zadatak4/SecureHost/Program.cs
@@ -10,6 +10,14 @@ namespace SecureHost
     {
         static void Main(string[] args)
         {
+            CommonService cs = new CommonService();
+            //cs.Modify("TestFile.txt", "Failed Test", WCFCommons.EModifyType.Overwrite);
+            cs.Create("TestFile.txt");
+            cs.Modify("TestFile.txt", "Test 1", WCFCommons.EModifyType.Append);
+            cs.Modify("TestFile.txt", "Test 2", WCFCommons.EModifyType.Append);
+            cs.Modify("TestFile.txt", "Test 3", WCFCommons.EModifyType.Overwrite);
+            Console.WriteLine(cs.Read("TestFile.txt"));
+            cs.Delete("TestFile.txt");
         }
     }
 }
